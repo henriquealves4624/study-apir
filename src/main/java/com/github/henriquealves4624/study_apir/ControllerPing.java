6b3895ccfcc26b3b9008426fe5f257592037c62e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 21:35:32 -0300

    Primeiro commit

---
+++ src/main/java/com/github/henriquealves4624/study_apir/ControllerPing.java
@@ -0,0 +1,20 @@
+package com.github.henriquealves4624.study_apir;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ping")
+public class ControllerPing {
+
+    @GetMapping
+    public String ping() {
+        return "pong";
+    }
+
+    public String mensagem() {
+        return "pong";
+    }
+
+}
