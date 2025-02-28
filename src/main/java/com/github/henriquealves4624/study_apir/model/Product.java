f8e531f1df49ac0b522e11caeba3583704dfef2e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 21:40:07 -0300

    feat: add list

--- src/main/java/com/github/henriquealves4624/study_apir/model/Product.java
+++ src/main/java/com/github/henriquealves4624/study_apir/model/Product.java
@@ -2,4 +2,23 @@ package com.github.henriquealves4624.study_apir.model;
 
 public class Product {
 
+    private Long id;
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 }
